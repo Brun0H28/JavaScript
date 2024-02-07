be0fa93e5972ee32e982db255e01f2d6e9e640f4
Bruno <158314045+Brun0H28@users.noreply.github.com>
2024-02-07 14:39:18 -0300

    Atividade01

--- 02-Aritimetico.js
+++ 02-Aritimetico.js
@@ -24,3 +24,11 @@ var MinhaVariavel = 10;
 var OutraVAriavel = 20;
 const _minhaConstante = 3.1514;
 var $minhaVariavel = 11;
+
+var v1 = 10;
+var v2 = 3;
+console.log("----------------------");
+console.log("valores: v1 =", v1, "| v2 =", v2);
+console.log("---------------------------");
+console.log("Resto da divisão inteira v1 % v2 =", v1 % v2);
+console.log("Expomenciação v1**v2 (v1 elevado à v2) =", v1 ** v2);
---
+++ Atividade1-Bruno.js
@@ -0,0 +1,42 @@
+var Nome = "Thiago Mazzi";
+var banco = "Nubank";
+var conta = "33";
+var numero = "96";
+var saldo = 500;
+
+console.log(
+  "Olá thiago mazzi, conta número",
+  conta,
+  ",Somos a",
+  banco,
+  "Agência",
+  numero,
+  "Seu saldo atual é de",
+  saldo,
+  "Reais"
+);
+saldo -= 150;
+console.log("------------------------------------");
+console.log("Compra realizada no macdonalds, no valor de 50 Reais");
+saldo -=50
+console.log("------------------------------------");
+console.log("Pix recebido de Bruno Martins no valor de 650 Reais");
+saldo += 650
+console.log("------------------------------------");
+console.log("compra realizada na Americanas, no valor de 500 Reais");
+saldo -= 500
+console.log("------------------------------------");
+console.log("Você, pagou sua fatura no valor de 20 Reais");
+saldo -= 20
+console.log("------------------------------------");
+console.log("você recebeu 500 Reais de Bruno Martins");
+saldo += 500
+console.log("------------------------------------");
+console.log(
+  "olá Thiago Mazzi, sua conta",
+  conta,
+  "Atualmente está com saldo de",
+  saldo,
+  " Reais. Obrigado pela preferência da Nubank",
+  numero
+);
